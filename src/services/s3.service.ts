2c5fa8eede5788719ac9519cdfe2e3eb5c107d77
winfredo <winfrednukpe2002@gmail.com>
2026-07-27 00:12:45 +0000

    fix: lazily construct the S3 client instead of at module load
    Constructing S3Client at import time meant any code path pulling in
    s3.service (including unrelated routes like /health) crashed if
    AWS_REGION wasn't set. Defer construction to first actual use, same
    pattern as the deferred mongoose.connect() in start().

--- src/services/s3.service.ts
+++ src/services/s3.service.ts
@@ -7,14 +7,21 @@ import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { randomUUID } from "crypto";
 import { env } from "../config/env";
 
-const s3 = new S3Client({ region: env.AWS_REGION });
+let s3: S3Client | undefined;
+
+function getClient(): S3Client {
+  if (!s3) {
+    s3 = new S3Client({ region: env.AWS_REGION });
+  }
+  return s3;
+}
 
 export async function uploadFile(
   buffer: Buffer,
   originalFilename: string
 ): Promise<string> {
   const key = `medical-history/${randomUUID()}-${originalFilename}`;
-  await s3.send(
+  await getClient().send(
     new PutObjectCommand({ Bucket: env.S3_BUCKET, Key: key, Body: buffer })
   );
   return key;
@@ -25,5 +32,5 @@ export async function getPresignedUrl(
   expiresIn = 3600
 ): Promise<string> {
   const command = new GetObjectCommand({ Bucket: env.S3_BUCKET, Key: key });
-  return getSignedUrl(s3, command, { expiresIn });
+  return getSignedUrl(getClient(), command, { expiresIn });
 }
